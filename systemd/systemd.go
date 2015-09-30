933c6e12e6e84d7717d5c05b374c60520aa2d310
Mrunal Patel <mrunalp@gmail.com>
2015-09-30 19:02:06 -0400

    Add support for mounting /etc/machine-id
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

--- systemd/systemd.go
+++ systemd/systemd.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -19,7 +21,7 @@ const (
 	journalDir = "/var/log/journal"
 )
 
-func setup(state *State) error {
+func setupJournal(state *State) error {
 	jDir := filepath.Join(journalDir, state.ID)
 	if err := os.MkdirAll(jDir, 0666); err != nil {
 		return err
@@ -37,7 +39,39 @@ func setup(state *State) error {
 	return nil
 }
 
-func teardown(state *State) error {
+func setupMachineID(state *State) error {
+	etcDir := filepath.Join("/tmp", state.Root, "/etc")
+	if err := os.MkdirAll(etcDir, 0666); err != nil {
+		return err
+	}
+	mFile := filepath.Join(etcDir, "machine-id")
+	if err := ioutil.WriteFile(mFile, []byte(state.ID), 0666); err != nil {
+		return err
+	}
+	cmFile := filepath.Join(state.Root, "/etc/machine-id")
+	f, err := os.Create(cmFile)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	flags := syscall.MS_BIND | syscall.MS_REC
+	if syscall.Mount(mFile, cmFile, "bind", uintptr(flags), ""); err != nil {
+		return err
+	}
+	return nil
+}
+
+func setup(state *State) error {
+	if err := setupJournal(state); err != nil {
+		return err
+	}
+	if err := setupMachineID(state); err != nil {
+		return err
+	}
+	return nil
+}
+
+func teardownJournal(state *State) error {
 	jcDir := filepath.Join(state.Root, journalDir, state.ID)
 	if err := syscall.Unmount(jcDir, syscall.MNT_DETACH); err != nil {
 		return err
@@ -49,6 +83,37 @@ func teardown(state *State) error {
 	jDir := filepath.Join(journalDir, state.ID)
 	if err := os.RemoveAll(jDir); err != nil {
 		return err
+	}
+	return nil
+}
+
+func teardownMachineID(state *State) error {
+	cmFile := filepath.Join(state.Root, "/etc/machine-id")
+	if err := syscall.Unmount(cmFile, syscall.MNT_DETACH); err != nil {
+		return err
+	}
+
+	etcDir := filepath.Join("/tmp", state.Root, "/etc")
+	mFile := filepath.Join(etcDir, "machine-id")
+	if err := os.Remove(mFile); err != nil {
+		return err
+	}
+	if err := os.RemoveAll(etcDir); err != nil {
+		return err
+	}
+	return nil
+}
+
+func teardown(state *State) error {
+	var err, err2 error
+	if err = teardownJournal(state); err != nil {
+		log.Printf("Journal teardown error: %q", err)
+	}
+	if err2 := teardownMachineID(state); err2 != nil {
+		log.Printf("Machine ID teardown error: %q", err)
+	}
+	if err != nil || err2 != nil {
+		return fmt.Errorf("Journal teardown error: %q, Machine ID teardown error: %q", err, err2)
 	}
 	return nil
 }
